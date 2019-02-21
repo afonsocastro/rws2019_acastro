dc8814a63ce82ee1293a9b8b6162255d21d0c169
AfonsoCastro <afonsocastro@ua.pt>
2019-02-21 10:57:13 +0000

    Second package

--- player_acastro/src/player_acastro.cpp
+++ player_acastro/src/player_acastro.cpp
@@ -1,7 +1,24 @@
+#include <ros/ros.h>
 #include <iostream>
 
-int main()
+const int MY_VAR = 2;
+struct A
 {
-    std::cout << "Hello World" << std::endl;
-    return 1;
+  int B;
+};
+
+int doSomething()
+{
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "player_acastro");
+  ros::NodeHandle n;
+  for (int i = 0; i < 10; i++)
+  {
+    std::cout << i << std::endl;
+  }
+  return 0;
 }
